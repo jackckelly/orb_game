0f74c10cb7141d3efca92beba68bd83cfc9221cf
Bill Marcy <wgmarcy@gmail.com>
2021-05-24 22:23:38 -0400

    Added snapping of orb collisions to nearest 90 degrees

--- scripts/Bouncer.gd
+++ scripts/Bouncer.gd
@@ -1,15 +1,11 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 export var velocity = Vector2()
 var accel_elapsed = 0
 export var accel_total_duration = 0.3
+
+var collision_angle = deg2rad(90)
 
 func _ready():
 	pass
@@ -28,6 +24,21 @@ func _physics_process(delta):
 			get_parent().get_parent().remove_child(get_parent())
 		elif not collide.get_collider().name == "Olauer":
 			velocity = velocity.bounce(collide.normal)
+			
+			# convert into polar coordinates
+			var r = velocity.length()
+			var theta = velocity.angle()
+
+			# print(rad2deg(theta))
+			# round the angle to the nearest multiple of 90 or 45
+			var rounded_theta = round (theta / collision_angle) * collision_angle
+			print(rounded_theta)
+			#print(rad2deg(rounded_theta))
+			# print(cos(rounded_theta))
+
+			# convert back to regular coordinates
+			velocity = Vector2(cos(rounded_theta) * r, sin(rounded_theta) * r)
+			# print(velocity)
 
 func _on_KillTimer_timeout():
 	get_parent().get_parent().remove_child(get_parent())

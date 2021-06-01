8e1280f984450080bd2d89034f3810ba806eb25b
Bill Marcy <wgmarcy@gmail.com>
2021-06-01 14:20:27 -0400

    Fix end screen for HTML; made cooldown timer slightly shorter

--- scripts/EndScreen.gd
+++ scripts/EndScreen.gd
@@ -3,6 +3,7 @@ extends Control
 onready var _sound = get_tree().get_root().get_node("Sound")
 
 func _ready():
+	get_tree().get_root().get_node("PauseScreen").enabled = false
 	_sound.get_node("Background Music").stop()
 
 func _process(delta):

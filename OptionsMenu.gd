09edc9335178784bfabaddfd46b2e057e105b34d
Jack Kelly <jackckelly936@gmail.com>
2021-06-03 23:33:50 -0700

    music/sound options

--- OptionsMenu.gd
+++ OptionsMenu.gd
@@ -1,16 +1,28 @@
 extends Control
 
-var options = ["Play", "Level Select", "Options", "Quit"]
+var options = ["Music", "Sounds", "Back"]
 var index = 0
 
 onready var selections = $HBoxContainer/Selections
+onready var music_label = $HBoxContainer/Labels/Music
+onready var sounds_label = $HBoxContainer/Labels/Sounds
 onready var _sound = get_tree().get_root().get_node("Sound")
-onready var level_manager = get_tree().get_root().get_node("LevelManager")
+
 
 func _ready():
-	_sound.try_play("Startup")
+	pass
 
 func _process(delta):
+	if _sound.is_music_enabled:
+		music_label.text = "MUSIC: ON"
+	else:
+		music_label.text = "MUSIC: OFF"
+	
+	if _sound.is_fx_enabled:
+		sounds_label.text = "SOUNDS: ON"
+	else:
+		sounds_label.text = "SOUNDS: OFF"
+	
 	if Input.is_action_just_pressed("ui_up") and index > 0:
 		_sound.try_play("Menu Move")
 		selections.get_child(index).get_child(0).visible = false
@@ -27,29 +39,17 @@ func _process(delta):
 	Input.is_action_just_pressed("ui_action"):
 		# once all menu options are available,
 		# we can uncomment this.
-		# _sound.try_play("Menu Select")
+		_sound.try_play("Menu Select")
 		var option = options[index]
 		
-		if option == "Play":
-			level_manager.load_level(0)
-			get_tree().get_root().get_node("PauseScreen").enabled = true
-			_sound.try_play("Menu Select")
-			_sound.try_play("Background Music")
+		if option == "Music":
+			_sound.is_music_enabled = !_sound.is_music_enabled
 			return
 		
-		if option == "Level Select":
-			_sound.try_play("Menu Select")
-			get_tree().change_scene("res://LevelSelect.tscn")
+		if option == "Sounds":
+			_sound.is_fx_enabled = !_sound.is_fx_enabled
 			return
 		
-		if option == "Options":
-			_sound.try_play("Menu Select")
-			get_tree().change_scene("res://OptionsMenu.tscn")
+		if option == "Back":
+			get_tree().change_scene("res://StartMenu.tscn")
 			return
-			
-		if option == "Quit":
-			if OS.get_name() == "HTML5":
-				_sound.try_play("Orb Wiff")
-			else:
-				_sound.try_play("Menu Select")
-				get_tree().quit()
